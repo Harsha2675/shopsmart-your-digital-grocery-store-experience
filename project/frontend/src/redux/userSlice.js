bf810a542248c289a2d77931e8c7ca40d5fee0e9
Harsha2675 <harshavardhankokkiripati@gmail.com>
2025-06-23 22:26:31 +0530

    Create userSlice.js

---
+++ project/frontend/src/redux/userSlice.js
@@ -0,0 +1,22 @@
+// src/redux/userSlice.js
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  user: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    loginUser: (state, action) => {
+      state.user = action.payload;
+    },
+    logoutUser: (state) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { loginUser, logoutUser } = userSlice.actions;
+export default userSlice.reducer;
