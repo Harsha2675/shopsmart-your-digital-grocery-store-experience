cadbacf0e93dcdbd9b01ead41eb02159fdb4e7f2
Harsha2675 <harshavardhankokkiripati@gmail.com>
2025-06-24 11:44:54 +0530

    Create Header.js

---
+++ project/frontend/src/component/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="bg-white shadow-md fixed w-full z-10 top-0 left-0">
+      <div className="p-4 text-xl font-bold">ShopSmart</div>
+    </header>
+  );
+}
+
+export default Header;
